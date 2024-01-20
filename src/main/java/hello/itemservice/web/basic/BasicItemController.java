6ff3e57b54c587b6355001c78e94fdeac2b517ea
SONGMIN <dhthdals@gmail.com>
2024-01-20 23:06:52 +0900

    RedirectAttributes - URL 인코딩, pathVariable, 쿼리파라미터

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -102,11 +103,19 @@ public class BasicItemController {
     } //String int 같은 단순 타입들이 오면 RequestParam
     //Item같은 객체가 오면 ModelAttribute가 적용된다.
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
     }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    } //치환되지 않은 status는 쿼리 파라미터로 넘어간다.
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
